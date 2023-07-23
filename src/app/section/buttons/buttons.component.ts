29aea12bbd82fa0c164c3aa727ff6a59e5feeb1b
Dongkeun <hongdongk@naver.com>
2023-07-23 22:09:10 +0900

    데이터바인딩1

--- src/app/section/buttons/buttons.component.ts
+++ src/app/section/buttons/buttons.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-buttons',
   templateUrl: './buttons.component.html',
-  styleUrls: ['./buttons.component.css']
+  styleUrls: ['./buttons.component.css'],
 })
 export class ButtonsComponent {
-
+  start($event: MouseEvent) {
+    console.log($event);
+  }
 }
--- src/app/section/time-display/time-display.component.ts
+++ src/app/section/time-display/time-display.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-time-display',
   templateUrl: './time-display.component.html',
-  styleUrls: ['./time-display.component.css']
+  styleUrls: ['./time-display.component.css'],
 })
 export class TimeDisplayComponent {
+  test = 0; // 컴포넌트에서 템플릿으로 단방향 데이터 바인딩
 
+  constructor() {
+    setInterval(() => {
+      this.test++;
+    }, 100000);
+  }
 }
