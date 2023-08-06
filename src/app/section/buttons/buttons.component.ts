4f1ca052c41eb8ac99215e4e2f8e779763ee4cc2
Dongkeun <hongdongk@naver.com>
2023-08-06 17:16:15 +0900

    데이터바인딩마무리

--- src/app/section/buttons/buttons.component.ts
+++ src/app/section/buttons/buttons.component.ts
@@ -8,11 +8,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class ButtonsComponent {
   @Output() clickEvent = new EventEmitter(); // 부모컴포넌트(secion)에 데이터 전달
 
-  alarm($event: MouseEvent) {
-    window.alert('경고');
-  }
-
-  start() {
-    this.clickEvent.emit('스타트'); // 부모컴포넌트(secion)에 데이터 전달 - clickEvent라는 이벤트를 통해 전달
+  executeButton(command: string) {
+    this.clickEvent.emit(command); // 부모컴포넌트(secion)에 데이터 전달 - clickEvent라는 이벤트를 통해 전달
   }
 }
--- src/app/section/time-display/time-display.component.ts
+++ src/app/section/time-display/time-display.component.ts
@@ -8,14 +8,47 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TimeDisplayComponent implements OnInit {
   @Input() inputData!: string; // 부모컴포넌트(secion)로부터 데이터 받기
 
-  title = '시간'; // 컴포넌트에서 템플릿으로 단방향 데이터 바인딩
+  min: number = 0; // 컴포넌트에서 템플릿으로 단방향 데이터 바인딩
+  sec: number = 0; // 컴포넌트에서 템플릿으로 단방향 데이터 바인딩
+  ms: number = 0; // 컴포넌트에서 템플릿으로 단방향 데이터 바인딩
+
+  timeInterval: any;
 
   ngOnInit(): void {}
 
   constructor() {}
 
+  timeStart() {
+    this.timeInterval = setInterval(() => {
+      this.ms++;
+    }, 10);
+  }
+
+  timeStop() {
+    clearInterval(this.timeInterval);
+  }
+
+  timeReset() {
+    this.timeStop();
+    this.ms = 0;
+  }
+
   // Input데이터값(inputData)값이 바뀌면 실행 - 컴포넌트여서 사용가능한 함수
-  ngOnChanges() {
-    console.log('바뀜');
+  ngOnChanges(changes: any) {
+    for (let propName in changes) {
+      if (propName == 'inputData') {
+        switch (changes[propName].currentValue) {
+          case 'start':
+            this.timeStart();
+            break;
+          case 'stop':
+            this.timeStop();
+            break;
+          case 'reset':
+            this.timeReset();
+            break;
+        }
+      }
+    }
   }
 }
