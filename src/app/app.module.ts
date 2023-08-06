8cc35c3b02b13aca292bdf1aeaa9957f03779421
Dongkeun <hongdongk@naver.com>
2023-08-06 17:46:02 +0900

    앱모듈화

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
+// 가장 처음실행되는 루트가 되는 모듈
 // 모든 컴포넌트는 모듈에 등록해야함!!
+// 사용하고 싶은 컴포넌트들 => 모듈에 declartions, 모듈 export => 그 모듈을 사용할려면 특정모듈에서 그 모듈 import
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -10,11 +12,27 @@ import { FooterComponent } from './footer/footer.component';
 import { SectionComponent } from './section/section.component';
 import { TimeDisplayComponent } from './section/time-display/time-display.component';
 import { ButtonsComponent } from './section/buttons/buttons.component';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, SectionComponent, TimeDisplayComponent, ButtonsComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  // 모듈에 컴포넌트를 선언해야 사용가능!(다른모듈에는 동일 컴포넌트 등록 불가능)
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SectionComponent,
+    TimeDisplayComponent,
+    ButtonsComponent,
+  ],
+
+  // 사용하고 싶은 모듈(안에 컴포넌트) 사용할 수 있도록 만들어줌
+  imports: [BrowserModule, AppRoutingModule, LayoutModule],
+
+  // 다른모듈들이 사용할 수 있도록 만들어줌
+  exports: [AppComponent],
+
   providers: [],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
